bcb7eed995ed03ec58fdab1141f3e0163b0729ea
Ivan <96693815+IvanBayern@users.noreply.github.com>
2023-02-24 10:54:15 +0100

    add c

---
+++ src/components/ForbesList/ForbesList.jsx
@@ -0,0 +1,34 @@
+import { ForbesListItem } from 'components';
+import {
+  BoardHeader,
+  LeaderBoard,
+  BoardTitle,
+  TitleTop,
+  TitleBottom,
+  LeaderBoardProfiles,
+} from './ForbesList.styled';
+
+export const ForbesList = ({ list }) => {
+  return (
+    <LeaderBoard>
+      <BoardHeader>
+        <BoardTitle>
+          <TitleTop>Forbes</TitleTop>
+          <TitleBottom>Leader board</TitleBottom>
+        </BoardTitle>
+      </BoardHeader>
+
+      <LeaderBoardProfiles>
+        {list.map(({ id, avatar, name, capital, isIncrease }) => (
+          <ForbesListItem
+            key={id}
+            avatar={avatar}
+            name={name}
+            capital={capital}
+            isIncrease={isIncrease}
+          />
+        ))}
+      </LeaderBoardProfiles>
+    </LeaderBoard>
+  );
+};
